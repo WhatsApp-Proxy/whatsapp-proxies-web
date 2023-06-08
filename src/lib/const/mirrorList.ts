3430f55f272aa6f34db7bb92570c07492ba4869e
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2023-06-08 12:51:20 +0200

    Update mirrorList.ts

--- src/lib/const/mirrorList.ts
+++ src/lib/const/mirrorList.ts
@@ -1,12 +1,4 @@
 export const MirrorSiteList = [
 	'https://whatsapp-proxy.com',
-	'https://whatsapp-proxy.online',
-	'https://whatsapp-proxy.net',
-	'https://whatsapp-proxy.xyz',
-	'https://whatsapp-proxies.com',
-	'https://whatsapp-proxies.net',
-	'https://whatsapp-proxies.org',
-	'https://whatsapp-proxies.info',
-	'https://whatsapp-proxies.online',
 	'https://wa-proxy.com'
 ];
